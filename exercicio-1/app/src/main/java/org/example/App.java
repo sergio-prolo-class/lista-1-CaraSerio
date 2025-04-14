44c5d23ef430823810e4a91517d241c9c3abff7e
Adriel Alves Ferreira de Souza <adriel.a2006@aluno.ifsc.edu.br>
2025-04-14 20:03:55 -0300

    Update App.java

--- exercicio-1/app/src/main/java/org/example/App.java
+++ exercicio-1/app/src/main/java/org/example/App.java
@@ -4,10 +4,7 @@
 package org.example;
 
 public class App {
-    public String getGreeting() {
-        return "Hello World!";
-    }
-
+   
 public static void main(String[] args) {
     
         if (args.length < 2) {
